f79867c8531d25fe0fb65e16f7090b4f0f800494
yongzheng <yongzheng@inspur.com>
2022-07-15 09:36:27 +0800

    change for test

---
+++ LYDproject/src/main/java/com/inspur/LYDproject/LyDprojectApplication.java
@@ -0,0 +1,13 @@
+package com.inspur.LYDproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyDprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyDprojectApplication.class, args);
+	}
+
+}
---
+++ LYDproject/src/test/java/com/inspur/LYDproject/LyDprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspur.LYDproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyDprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
